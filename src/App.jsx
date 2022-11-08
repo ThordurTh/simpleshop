684f0c5f3133f4b02ca9193cb36faf6e2f6e2f0d
thordurf <thordur921@gmail.com>
2022-11-08 13:11:47 +0100

    scaffolding

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+import { useState, useEffect } from "react";
+import Header from "./components/Header";
+import ProductList from "./components/ProductList";
+import Basket from "./components/Basket";
+
 function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    async function getData() {
+      const res = await fetch("https://kea-alt-del.dk/t7/api/products");
+      const data = await res.json();
+      setProducts(data);
+    }
+    getData();
+  }, []);
   return (
     <div className="App">
-      <div></div>
+      <Header />
+      <ProductList products={products} />
+      <Basket products={products} />
     </div>
   );
 }
---
+++ src/components/Basket.jsx
@@ -0,0 +1,12 @@
+function Basket() {
+  return (
+    <section className="Basket">
+      <ul>
+        <li>Someproduct x 3, 1234,-</li>
+        <button>Buy now</button>
+      </ul>
+    </section>
+  );
+}
+
+export default Basket;
